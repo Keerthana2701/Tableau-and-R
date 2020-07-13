2462719dcd54dc4436c37ce9785a1ad04835cf8a
Keerthana <67209958+Keerthana-NeverGiveUp@users.noreply.github.com>
2020-07-13 16:42:43 -0700

    Add files via upload

---
+++ Tableau R Connection.R
@@ -0,0 +1,11 @@
+library(Rserve)
+library(psych)
+install.packages('Rserve')
+install.packages("Rserve")
+Rserve()
+
+## for Time forecast in tableau
+install.packages('forecast')
+
+install.packages('mvoutlier')
+library(mvoutlier)
